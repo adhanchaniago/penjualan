87f1bee1ded9c17f8f2c481ca86a489e24a64068
ridwanzal <zalbinaridwan@gmail.com>
2020-02-25 20:44:08 +0700

    add all

--- application/views/admin/dynamic_pembukuan.php
+++ application/views/admin/dynamic_pembukuan.php
@@ -64,177 +64,143 @@
             ?>
             <h4>Buku Besar</h4>
             <br/>
+            <button class="btn btn-primary btn-sm " id="btn_kas" type="submit">Kas</button>
+            <button class="btn btn-primary btn-sm " id="btn_penjualan" type="submit">Penjualan</button>
+            <br/>
+            <br/>
+            <br/>
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-xs-12 float-right">
                     <button class="btn btn-sm btn-secondary" id="print_pdf">PDF</button>
                 </div>
             </div>
             <br/>
-            <div class="row">
-            <div class="col-lg-12 col-md-12">
-                <!-- <h4>Kas</h4>
-                <table id="table1" class="table table-bordered responsive table-hover" width="100%">
-                <thead>
-                    <tr style="background-color:#ffffff;">
-                        <th>Tanggal</th>
-                        <th>Debit</th>
-                        <th>Tanggal</th>
-                        <th>Kredit</th>
-                    </tr>
-                </thead>        
-                        <tbody>
-                                <tr>
-                                    <td>
-                                        <?php if(isset($tanggal_akhir) && !empty($tanggal_akhir)){
-                                            echo convBulan($tanggal_akhir[0]->bulan) .' '. $tanggal_akhir[0]->tahun;
-                                        }?>
-                                    </td>
-                                    <td>
-                                        <?php
-                                             if(isset($total_tagihan) && isset($total_dp)){
-                                                 $totalse = $total_tagihan[0]->total_tagihan + $total_dp[0]->total_dp;
-                                                 echo rupiah($totalse);
-                                             }
-                                        ?>
-                                    </td>
-                                    <td> - </td>
-                                    <td>Rp. 0,00</td>
-                                </tr>
-                                <tr style="background-color:#eee;">
-                                    <td>Sisa Saldo</td>
-                                    <td>
-                                        <?php
-                                             if(isset($total_tagihan) && isset($total_dp)){
-                                                 $totalse = $total_tagihan[0]->total_tagihan + $total_dp[0]->total_dp;
-                                                 echo rupiah($totalse);
-                                             }
-                                        ?>
-                                    </td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                        </tbody>
-                </table> -->
-                <h4>Kas</h4>
-                <table id="table1" class="table table-bordered responsive table-hover tablepembukuan" width="100%">
-                <thead>
-                    <tr style="background-color:#ffffff;">
-                        <th>Tanggal</th>
-                        <th>Debit</th>
-                        <th>Tanggal</th>
-                        <th>Kredit</th>
-                    </tr>
-                </thead>        
-                        <tbody>
-                                 <?php 
-                                    $check = isset($daftar_jurnal) && isset($total_tagihan) && isset($total_dp);
-                                    if($check){
-                                        $i = 0;
-                                        $total_kredit = 0;
-                                        $total_uang = 0;
-                                        $totalse = $total_tagihan[0]->total_tagihan + $total_dp[0]->total_dp;
-                                        foreach($daftar_jurnal as $list){
-                                            $i++;
-                                            $total_kredit =  $total_kredit  + $list->pembayaran_perbulan;
-                                            $total_uang = $list->pembayaran_perbulan;
-                                            $split_date = explode(' ', $list->tanggal_tagihan);
-                                            ?>
-                                                <tr>
-                                                <td><?php echo $split_date[0]; ?></td>
-                                                <td><?php echo rupiah($list->pembayaran_perbulan); ?></td>
+            <div class="row" id="tabkas">
+                <div class="col-lg-12 col-md-12">
+                    <h4>Kas</h4>
+                    <table id="table1" class="table table-bordered responsive table-hover tablepembukuan" width="100%">
+                    <thead>
+                        <tr style="background-color:#ffffff;">
+                            <th>Tanggal</th>
+                            <th>Debit</th>
+                            <th>Tanggal</th>
+                            <th>Kredit</th>
+                        </tr>
+                    </thead>        
+                            <tbody>
+                                    <?php 
+                                        $check = isset($daftar_jurnal) && isset($total_tagihan) && isset($total_dp);
+                                        if($check){
+                                            $i = 0;
+                                            $total_kredit = 0;
+                                            $total_uang = 0;
+                                            $totalse = $total_tagihan[0]->total_tagihan + $total_dp[0]->total_dp;
+                                            foreach($daftar_jurnal as $list){
+                                                $i++;
+                                                $total_kredit =  $total_kredit  + $list->pembayaran_perbulan;
+                                                $total_uang = $list->pembayaran_perbulan;
+                                                $split_date = explode(' ', $list->tanggal_tagihan);
+                                                ?>
+                                                    <tr>
+                                                    <td><?php echo $split_date[0]; ?></td>
+                                                    <td><?php echo rupiah($list->pembayaran_perbulan); ?></td>
+                                                        <td><?php 
+                                                                if($i > 1){
+                                                                    echo ""; 
+                                                                }else{
+                                                                    echo $split_date[0];
+                                                                }
+                                                            ?>
+                                                    </td>
                                                     <td><?php 
-                                                            if($i > 1){
-                                                                echo ""; 
-                                                            }else{
-                                                                echo $split_date[0];
-                                                            }
-                                                        ?>
-                                                  </td>
-                                                  <td><?php 
-                                                            if($i > 1){
-                                                                echo ""; 
-                                                            }else{
-                                                                echo rupiah($totalse);
-                                                            }
-                                                        ?>
-                                                  </td>
-                                                </tr>
-                                        <?php }?>
-                                    <?php }else{
+                                                                if($i > 1){
+                                                                    echo ""; 
+                                                                }else{
+                                                                    echo rupiah($totalse);
+                                                                }
+                                                            ?>
+                                                    </td>
+                                                    </tr>
+                                            <?php }?>
+                                        <?php }else{
+                                            ?>
+                                                <tr></tr>
+                                            <?php
+                                        }
                                         ?>
-                                            <tr></tr>
-                                        <?php
-                                    }
+                                        <tr style="background-color:#f9f9f9;">   
+                                        <td>Total</td>
+                                        <td></td>
+                                        <td><?php echo rupiah($total_kredit); ?></td>
+                                        <td><?php echo rupiah($totalse); ?></td>
+                                        </tr>
+                                        <tr style="background-color:#efefef;">   
+                                        <td>Sisa Saldo</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td> <?php 
+                                            $sisa_saldo = $totalse - $total_kredit;
+                                            echo ''.rupiah($sisa_saldo); ?>
+                                            </td>
+                                        </tr>
+                            </tbody>
+                    </table>
+                </div>
+            </div>
+                    <br/>
+                    <br/>
+            <div class="row" id="tabjual">
+                <div class="col-lg-12 col-md-12 col-xs-12">
+                    <h4>Penjualan</h4>
+                    <table id="table3" class="table table-bordered responsive tablepembukuan" width="100%">
+                    <thead>
+                        <tr style="background-color:#ffffff;">
+                            <th>Tanggal</th>
+                            <th>Debit</th>
+                            <th>Tanggal</th>
+                            <th>Kredit</th>
+                        </tr>
+                    </thead>        
+                            <tbody>
+                                    <?php 
+                                        $check3 = isset($daftar_jurnal);
+                                        if($check3){
+                                            $i = 0;
+                                            $total_penjualan = 0;
+                                            foreach($daftar_jurnal as $list){
+                                                $i++;
+                                                $total_penjualan =  $total_penjualan  + $list->pembayaran_perbulan;
+                                                ?>
+                                                    <tr>
+                                                    <td><?php echo '-'; ?></td>
+                                                    <td><?php echo '-'; ?></td>
+                                                    <td><?php echo $list->tanggal_tagihan; ?></td>
+                                                    <td><?php echo rupiah($list->pembayaran_perbulan); ?></td>
+                                                    </tr>
+                                            <?php }?>
+                                        <?php }
                                     ?>
                                     <tr style="background-color:#f9f9f9;">   
-                                       <td>Total</td>
-                                       <td></td>
-                                       <td><?php echo rupiah($total_kredit); ?></td>
-                                       <td><?php echo rupiah($totalse); ?></td>
+                                        <td>Total</td>
+                                        <td></td>
+                                        <td><?php echo "" ?></td>
+                                        <td><?php echo rupiah($total_penjualan);?></td>
                                     </tr>
                                     <tr style="background-color:#efefef;">   
-                                       <td>Sisa Saldo</td>
-                                       <td></td>
-                                       <td></td>
-                                       <td> <?php 
-                                           $sisa_saldo = $totalse - $total_kredit;
-                                           echo ''.rupiah($sisa_saldo); ?>
-                                        </td>
+                                        <td>Sisa Saldo</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td> 
+                                                <?php 
+                                                $sisa_saldo = $total_penjualan;
+                                                echo ''.rupiah($sisa_saldo); ?>
+                                            </td>
                                     </tr>
-                        </tbody>
-                </table>
-                <br/>
-                <br/>
-                <h4>Penjualan</h4>
-                <table id="table3" class="table table-bordered responsive tablepembukuan" width="100%">
-                <thead>
-                    <tr style="background-color:#ffffff;">
-                        <th>Tanggal</th>
-                        <th>Debit</th>
-                        <th>Tanggal</th>
-                        <th>Kredit</th>
-                    </tr>
-                </thead>        
-                        <tbody>
-                                 <?php 
-                                    $check3 = isset($daftar_jurnal);
-                                    if($check3){
-                                        $i = 0;
-                                        $total_penjualan = 0;
-                                        foreach($daftar_jurnal as $list){
-                                            $i++;
-                                            $total_penjualan =  $total_penjualan  + $list->pembayaran_perbulan;
-                                            ?>
-                                                <tr>
-                                                  <td><?php echo '-'; ?></td>
-                                                  <td><?php echo '-'; ?></td>
-                                                  <td><?php echo $list->tanggal_tagihan; ?></td>
-                                                  <td><?php echo rupiah($list->pembayaran_perbulan); ?></td>
-                                                </tr>
-                                        <?php }?>
-                                    <?php }
-                                 ?>
-                                 <tr style="background-color:#f9f9f9;">   
-                                    <td>Total</td>
-                                    <td></td>
-                                    <td><?php echo "" ?></td>
-                                    <td><?php echo rupiah($total_penjualan);?></td>
-                                 </tr>
-                                 <tr style="background-color:#efefef;">   
-                                    <td>Sisa Saldo</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td> 
-                                            <?php 
-                                            $sisa_saldo = $total_penjualan;
-                                            echo ''.rupiah($sisa_saldo); ?>
-                                        </td>
-                                 </tr>
-                        </tbody>
-                </table>
+                            </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-  </div>
+    </div>
 </div>
 </main>
 <script>
@@ -283,6 +249,21 @@
                 });
               doc.save('Laporan Buku Besar.pdf');
               doc.showHead('firstPage');  
-        });
+
+
+              
+            });
+
+            $('#tabjual').hide();
+
+            $('#btn_kas').on('click', function(){
+                $('#tabkas').show();
+                $('#tabjual').hide();
+            });    
+
+            $('#btn_penjualan').on('click', function(){
+                $('#tabkas').hide();
+                $('#tabjual').show();
+            });    
     });
 </script>
